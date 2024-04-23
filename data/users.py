cd681d9f117fd722fec074e2a7fca317f3705942
cstlore <ok15022000@gmail.com>
2024-04-23 21:01:50 +0400

    Create project

---
+++ data/users.py
@@ -0,0 +1,24 @@
+import datetime
+import hashlib
+
+import sqlalchemy
+from flask_login import UserMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    def check_password(self, password):
+        return password == self.hashed_password
